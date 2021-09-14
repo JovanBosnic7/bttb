27d46127a2cb48c5d1f37e713689c70ef1d0eaff
Jovan Bosnic <jovan.bosnic_7@uns.ac.rs>
2021-09-14 23:51:01 +0200

    Add css

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -3,13 +3,20 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
-  styleUrls: ['./home-page.component.css']
+  styleUrls: [
+    './home-page.component.css',
+    './novi.css',
+    './novi2.css',
+    './novi3.css',
+    './novi4.css',
+    './novi5.css',
+    './novi6.css',
+    './novi7.css',
+    './novi8.css',
+  ],
 })
 export class HomePageComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
